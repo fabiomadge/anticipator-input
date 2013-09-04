ba32f48d8d7c1abb2cf643c10d46e4e8e04b47c1
Fabio Madge-Pimentel <fabio@madge.me>
2013-09-04 23:49:44 +0200

    added app name for OSX

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main
 {
 	public static void main(String[] s) throws Exception{
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "anticipator-input");
 		Logic l = new Logic();
 	}
 }
